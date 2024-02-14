dc41924db884af7194f1e5f27d3aacdd3a0c0a6a
Marlon Prado - Ubuntu <marlonprado04@gmail.com>
2024-02-13 22:58:24 -0300

    feat: add parser

--- userdept/src/main/java/com/marlonprado04/userdept/UserdeptApplication.java
+++ userdept/src/main/java/com/marlonprado04/userdept/UserdeptApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserdeptApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserdeptApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UserdeptApplication.class, args);
+  }
 }
